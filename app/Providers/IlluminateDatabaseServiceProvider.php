a631c4efcee0a69fc80569fc9e67aefe1353efe6
Raphael Marco <raphaelmarco@outlook.com>
2016-02-14 16:34:11 +0800

    Change contact email in author part

--- app/Providers/IlluminateDatabaseServiceProvider.php
+++ app/Providers/IlluminateDatabaseServiceProvider.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Boardy forum software
  *
- * (c) Raphael Marco <raphael.marco@hotmail.ph>
+ * (c) Raphael Marco <raphaelmarco@outlook.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
