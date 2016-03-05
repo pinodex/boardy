b12fe8b82cca30e19de1549b09c9ef3ecc57f86a
Raphael Marco <raphaelmarco@outlook.com>
2016-03-05 23:44:15 +0800

    Added Users model

---
+++ app/Models/Users.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Boardy forum software
+ *
+ * (c) Raphael Marco <raphaelmarco@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boardy\Models;
+
+use Boardy\Services\Hash\Hash;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Users extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $table = 'users';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = ['name', 'username', 'email', 'password'];
+
+    /**
+     * Allows for automatic password hashing
+     *
+     * @param string $password User's password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
+
+}
